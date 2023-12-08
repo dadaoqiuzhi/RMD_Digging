0523b467824e7d1e8f68a27a50fe2d64b83af5fe
dadaoqiuzhi <56991198+dadaoqiuzhi@users.noreply.github.com>
2023-12-08 18:28:17 +0800

    tiny upgrade

---
+++ chemi_mechanism/Deconvolu_SysConvert.m
@@ -0,0 +1,11 @@
+%This function is used to transform a number to decimal system.
+function num_10base = Deconvolu_SysConvert(given_num_char,base)
+num_table = {'1','2','3','4','5','6','7','8','9','a','b','c','d',...
+    'e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'};
+[~,matches] = strsplit(given_num_char,num_table,'CollapseDelimiters',false);
+num_10base = 0;
+for i = 1:length(matches)
+    [~,num_true] = ismember(matches{i},num_table);
+    num_10base = num_10base + num_true*base^(length(matches)-i);
+end
+end
